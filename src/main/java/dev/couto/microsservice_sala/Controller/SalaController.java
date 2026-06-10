04066d027b922c5e461772fddd51ef59623c0c42
cauacouto <cauacouto022@gmail.com>
2026-06-10 10:27:49 -0300

    feat: adiciona métodos de listagem, atualização, inativação e exclusão de salas no SalaController

--- src/main/java/dev/couto/microsservice_sala/Controller/SalaController.java
+++ src/main/java/dev/couto/microsservice_sala/Controller/SalaController.java
@@ -5,19 +5,18 @@ import dev.couto.microsservice_sala.Dto.SalaRequestDto;
 import dev.couto.microsservice_sala.Dto.SalaResponseDto;
 import dev.couto.microsservice_sala.Service.SalaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sala")
 @RequiredArgsConstructor
 public class SalaController {
 
-    private final SalaService salaService;
+   private final SalaService salaService;
 
 
     @PostMapping
@@ -25,4 +24,28 @@ public class SalaController {
         var request = salaService.criarSala(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(request);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<SalaResponseDto>> listarSalas( Pageable pageable){
+        return ResponseEntity.ok(salaService.listarSalas(pageable));
+
+
+    }
+    @PatchMapping
+    public ResponseEntity<SalaResponseDto> atualizarSala(@RequestBody SalaRequestDto request,@PathVariable Integer id){
+        this.salaService.atualizarSala(request, id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{id}/inativar")
+    public ResponseEntity<Void> inativar(@PathVariable Integer id){
+        this.salaService.inativarSala(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarSala(Integer id){
+        this.salaService.deletarSala(id);
+        return ResponseEntity.noContent().build();    }
+
 }
