307667970ba682d0d9e64fb3d83f1f10679773be
cauacouto <cauacouto022@gmail.com>
2026-06-09 21:47:01 -0300

    initialize project structure with Spring Boot and Maven configuration

---
+++ src/main/java/dev/couto/microsservice_sala/MicrosserviceSalaApplication.java
@@ -0,0 +1,13 @@
+package dev.couto.microsservice_sala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosserviceSalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosserviceSalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/couto/microsservice_sala/MicrosserviceSalaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.couto.microsservice_sala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosserviceSalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
