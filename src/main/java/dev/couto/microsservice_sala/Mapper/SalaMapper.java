3813beb295b831fef2fceee3ea6fea3f330851d6
cauacouto <cauacouto022@gmail.com>
2026-06-11 19:37:40 -0300

    add anotacao mapping

--- src/main/java/dev/couto/microsservice_sala/Mapper/SalaMapper.java
+++ src/main/java/dev/couto/microsservice_sala/Mapper/SalaMapper.java
@@ -4,12 +4,14 @@ import dev.couto.microsservice_sala.Dto.SalaRequestDto;
 import dev.couto.microsservice_sala.Dto.SalaResponseDto;
 import dev.couto.microsservice_sala.domin.Sala;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SalaMapper {
 
 
 
+  @Mapping(target = "status",source = "statusSala")
   SalaResponseDto toDto(Sala entity);
 
 
